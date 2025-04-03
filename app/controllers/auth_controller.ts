bede29f340079e695d9b28cb06d187a60ea2785b
Daimus Suudi <daimus.suudi@gmail.com>
2025-04-03 13:32:31 +0700

    chore: add logging to auth routes and user services

--- app/controllers/auth_controller.ts
+++ app/controllers/auth_controller.ts
@@ -3,12 +3,14 @@ import { loginValidator, registerValidator } from '#validators/auth'
 import User from '#models/user'
 import UserService from '#services/user_service'
 import { inject } from '@adonisjs/core'
+import logger from '@adonisjs/core/services/logger'
 
 @inject()
 export default class AuthController {
   constructor(private userService: UserService) {}
 
   async register({ request, response }: HttpContext) {
+    logger.info('POST /api/v1/auth/register')
     const payload = await request.validateUsing(registerValidator)
     const user = await this.userService.createUser(payload)
 
@@ -16,6 +18,7 @@ export default class AuthController {
   }
 
   async login({ request, response }: HttpContext) {
+    logger.info('POST /api/v1/auth/login')
     const { email, password } = await request.validateUsing(loginValidator)
 
     const user = await User.verifyCredentials(email, password)
@@ -28,6 +31,7 @@ export default class AuthController {
   }
 
   async logout({ auth, response }: HttpContext) {
+    logger.info('POST /api/v1/auth/logout')
     const user = auth.getUserOrFail()
     const token = auth.user?.currentAccessToken.identifier
     if (!token) {
--- app/services/user_service.ts
+++ app/services/user_service.ts
@@ -1,7 +1,9 @@
 import User from '#models/user'
+import logger from '@adonisjs/core/services/logger'
 
 export default class UserService {
   public async createUser(data: Partial<User>) {
+    logger.info('USER_SERVICE/createUser')
     return await User.create(data)
   }
 }
